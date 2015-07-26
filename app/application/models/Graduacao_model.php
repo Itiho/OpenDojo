998c73bbb37faa6f73572b93f2d8cab9b5ad58ec
Willian Itiho Amano <itihoitiho@gmail.com>
2015-07-26 13:38:18 -0400

    Limpeza de código

--- app/application/models/Graduacao_model.php
+++ app/application/models/Graduacao_model.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  * -------------------------------------------------------
@@ -18,25 +17,14 @@ class Graduacao_Model extends MY_Model {
     public $primary_key = 'idGraduacao';
     
     protected $rules = array(
-        'insert' => array(
             'nomeGraduacao' => array('field'=>'nomeGraduacao',
                 'label'=>'Nome',
-                'rules'=>'required|min_length'),
+                'rules'=>'required|min_length[3]'),
             'arteMarcial' => array('field'=>'arteMarcial',
                 'label'=>'Arte marcial',
-                'rules'=>'required|greater_than[3]',
-                'errors' => array('greater_than' => '{field} é obrigatório'))
-        ),
-        'update' => array(
-            'nomeGraduacao' => array('field'=>'nomeGraduacao',
-                'label'=>'Nome',
-                'rules'=>'required')
-        )
-    );
-
+                'rules'=>'required'));
 
     function __construct() {
-//        $this->has_one['arteMarcial_fk'] = 'ArteMarcial_Model';
         $this->timestamps = FALSE;
         $this->has_one['arteMarcial_fk'] = array('ArteMarcial_Model','idArteMarcial','arteMarcial');
         parent::__construct();
@@ -44,20 +32,5 @@ class Graduacao_Model extends MY_Model {
         $this->pagination_delimiters = array('<li>', '</li>');
         $this->pagination_arrows = array('&lt;', '&gt;');
     }
-
-    public function get_graduacoes() {
-
-        $query = $this->db
-                ->select('idGraduacao, nomeGraduacao, nomeArteMarcial')
-                ->from($this->_table)
-                ->join('ArteMarcial', 'Graduacao.arteMarcial = ArteMarcial.idArteMarcial')
-                ->order_by('idGraduacao')
-                ->get();
-        return $query->result();
-    }
-   
-
-
 }
-
 ?>
