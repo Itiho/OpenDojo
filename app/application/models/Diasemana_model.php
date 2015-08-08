13551f1f2f6b23e71cb1a6ac86bee0749c17051d
Willian Itiho Amano <itihoitiho@gmail.com>
2015-08-08 16:09:20 -0400

    Modelo estático para dia da semana

---
+++ app/application/models/Diasemana_model.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ *
+ * -------------------------------------------------------
+ * CLASSNAME:        Academia_Model
+ * FOR MYSQL TABLE:  Academia
+ * FOR MYSQL DB:     OpenDojo
+ * -------------------------------------------------------
+ */
+
+class DiaSemana_Model extends MY_Model {
+
+    private $diasSemanas = array(
+        '1' => 'Domingo',
+        '2' => 'Segunda',
+        '3' => 'Terça',
+        '4' => 'Quarta',
+        '5' => 'Quinta',
+        '6' => 'Sexta',
+        '7' => 'Sábado');
+
+    public function get_diasSemana() {
+        return $this->diasSemanas;
+    }
+
+    public function get_dia($nome) {
+        return array_search($nome, $this->diasSemanas, true);
+    }
+
+    public function get_diaNome($dia) {
+        if(isset($this->diasSemanas[strtoupper($dia)])) {
+            return $this->diasSemanas[strtoupper($dia)];
+        } else {
+            return NULL;
+        }
+    }
+
+}
+?>
+
