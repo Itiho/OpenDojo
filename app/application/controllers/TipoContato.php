eafee8e0f9c51779e9c03d4b4ca696035f38929b
Willian Itiho Amano <itihoitiho@gmail.com>
2015-09-08 09:00:45 -0400

    Funcionando CRUD do tipo de contato

--- app/application/controllers/TipoContato.php
+++ app/application/controllers/TipoContato.php
@@ -38,51 +38,51 @@ class TipoContato extends CI_Controller {
     }
 
     function edit($id = 0) {
-        $this->data['artemarcial'] = $this->tipocontato_model->as_array()->get($id);
+        $this->data['tipocontato'] = $this->tipocontato_model->as_array()->get($id);
         if (count($this->input->post()) == 0) {
-            $this->load->view('ArteMarcialEdit_view', $this->data);
+            $this->load->view('TipoContatoEdit_view', $this->data);
         } else {
-            $artemarcial = $this->input->post();
+            $tipocontato = $this->input->post();
             $this->form_validation->set_rules($this->tipocontato_model->rules);
             if ($this->form_validation->run() == TRUE) {
-                $resultado = $this->tipocontato_model->update($artemarcial, $this->input->post('idArteMarcial'));
-                $this->session->set_flashdata('message', 'Arte marcial "' . $this->input->post('nomeArteMarcial') . '" editada com sucesso');
+                $resultado = $this->tipocontato_model->update($tipocontato, $this->input->post('idTipoContato'));
+                $this->session->set_flashdata('message', 'Tipo de contato "' . $this->input->post('tipoContato') . '" editado com sucesso');
                 $this->session->set_flashdata('type_message', '1'); //Sucesso
-                redirect('/ArteMarcial');
+                redirect('/TipoContato');
             } else {
-                $this->data['artemarcial'] = $artemarcial;
-                $this->load->view('ArteMarcialEdit_view', $this->data);
+                $this->data['tipocontato'] = $tipocontato;
+                $this->load->view('TipoContatoEdit_view', $this->data);
             }
         }
     }
 
     function add() {
         if (count($this->input->post()) == 0) {
-            $this->load->view('ArteMarcialAdd_view', $this->data);
+            $this->load->view('TipoContatoAdd_view', $this->data);
         } else {
             $resultado = $this->tipocontato_model->from_form()->insert();
             if ($resultado) {
-                $this->session->set_flashdata('message', 'Arte marcial "' . $this->input->post('nomeArteMarcial') . '" cadastrada com sucesso');
+                $this->session->set_flashdata('message', 'Tipo de contato "' . $this->input->post('tipoContato') . '" cadastrado com sucesso');
                 $this->session->set_flashdata('type_message', '1'); //Sucesso
-                redirect('/ArteMarcial');
+                redirect('/TipoContato');
             } else {
-                $this->data['artemarcial'] = $this->input->post();
-                $this->load->view('ArteMarcialAdd_view', $this->data);
+                $this->data['tipocontato'] = $this->input->post();
+                $this->load->view('TipoContatoAdd_view', $this->data);
             }
         }
     }
 
     function delete($id = null) {
         if (isset($id)) {
-            $artemarcial = $this->tipocontato_model->fields('nomeArteMarcial')->get($id);
+            $tipocontato = $this->tipocontato_model->fields('tipoContato')->get($id);
             if ($this->tipocontato_model->delete($id)) {
-                $this->session->set_flashdata('message', 'Arte marcial "' . $artemarcial->nomeArteMarcial . '" deletada com sucesso');
+                $this->session->set_flashdata('message', 'Tipo de contato "' . $tipocontato->tipoContato . '" deletado com sucesso');
                 $this->session->set_flashdata('type_message', '1'); //Sucesso
             } else {
-                $this->session->set_flashdata('message', 'Arte marcial "' . $artemarcial->nomeArteMarcial . '" não pôde ser deletada');
+                $this->session->set_flashdata('message', 'Arte marcial "' . $tipocontato->tipoContato . '" não pôde ser deletado');
                 $this->session->set_flashdata('type_message', '0'); //Erro
             }
-            redirect('/ArteMarcial');
+            redirect('/TipoContato');
         }
     }
 
---
+++ app/application/views/TipoContatoEdit_view.php
@@ -0,0 +1,58 @@
+<?php
+    $this->view('Header_view');
+        
+    //Declarações de tipo para formulário
+    $attributes_form = array('class' => 'form-horizontal');
+    $attributes_label = array('class' => 'col-sm-2 control-label');
+    $attributes_text = array('class' => 'form-control');
+    $attributes_dropdown = 'class="form-control"';
+    $attributes_submit = array('class' => 'btn btn-default');
+?>
+<div class="container-fluid">
+    <h1><i class="fa fa-pencil  fa-3x"></i> <?=$cabecalho ?></h1>
+
+<?php
+    echo form_open('TipoContato/edit/'.$tipocontato['idTipoContato'],$attributes_form);
+    echo form_hidden('idTipoContato',$tipocontato['idTipoContato']);
+            
+    if(form_error('tipoContato')){
+        echo '<div class="form-group has-error">';
+    } else{
+        echo '<div class="form-group">';
+    }
+
+    echo form_label('Arte marcial', 'tipoContato', $attributes_label);
+?>
+    <div class="col-xs-4">
+<?php
+    //atributos extra do campo texto
+    $attributes_text['name'] = 'tipoContato';
+    $attributes_text['id']  = 'tipoContato';
+    $attributes_text['value'] = $tipocontato['tipoContato'];
+    echo form_input($attributes_text);
+?>
+</div>
+<?php
+    echo form_error('tipoContato');
+?>
+</div>
+
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+<?php
+    echo form_submit($attributes_submit, 'Salvar'); 
+            
+    $options = 'onClick="window.location=\''.site_url('TipoContato').'\'" class="btn"';
+    echo form_button('cancel', 'Cancelar', $options); 
+?>
+    </div>
+  </div>
+
+
+<?php 
+    echo form_close(); 
+?>
+</div>
+<?php
+	$this->view('Footer_view');
+?>
